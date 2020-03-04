d593dcddd8ac94f3d59392ec2544b88e297b27eb
Desmond Lee <desmond@le3.io>
2020-03-03 23:14:33 -0600

    first commit
    first commit

--- app/src/components/Board.tsx
+++ app/src/components/Board.tsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { Action } from '../generated';
+import { Grid, Button } from '@material-ui/core'
 
 type Props = {
     sprintId: string
     actions: Pick<Action, 'id' | 'type' | 'timestamp' | 'payload'>[]
 }
 
+
 const Board: React.FC<Props> = () => {
     return <>
-        loaded!
+        <Grid container>
+            <Button> click here to add a stupid card </Button>
+            <Grid item xs={4}>
+                HAHAHAHAHA
+            </Grid>
+            <Grid item xs={4}>
+                HAHAHAHAHA
+            </Grid>
+            <Grid item xs={4}>
+                HAHAHAHAHA
+            </Grid>
+        </Grid>
     </>
 }
 
